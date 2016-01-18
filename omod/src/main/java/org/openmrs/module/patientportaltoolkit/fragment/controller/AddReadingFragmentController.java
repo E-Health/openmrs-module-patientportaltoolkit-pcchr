b86c3f659caeb7d0e5b200a0d3c9321e3c30c99d
Bellraj Eapen <github.gulfdoctor.net>
2016-01-17 19:52:32 -0500

    HomePageController added

--- omod/src/main/java/org/openmrs/module/patientportaltoolkit/fragment/controller/AddReadingFragmentController.java
+++ omod/src/main/java/org/openmrs/module/patientportaltoolkit/fragment/controller/AddReadingFragmentController.java
@@ -25,32 +25,12 @@ public class AddReadingFragmentController {
 
 
     public void controller(FragmentModel model) {
-        Patient patient;
-        Person person;
-        PatientService patientService = Context.getPatientService();
 
-        person = Context.getAuthenticatedUser().getPerson();
-        User creator = person.getCreator();
-        patient= Context.getPatientService().getPatientByUuid(Context.getAuthenticatedUser().getPerson().getUuid());
-        if (patient == null) {
-            //patient= Context.getPatientService().getPatient(7); //For Testing
-            //Create a new patient
-            // https://wiki.openmrs.org/questions/79660918/creating-a-patient-from-a-module
-            // OpenMRS ID set as Not required in database
-            patient = new Patient(person);
-            PatientIdentifierType PIT = patientService.getPatientIdentifierTypeByName("Old Identification Number");
-            PatientIdentifier pI = new PatientIdentifier();
-            pI.setCreator(creator);
-            pI.setIdentifierType(PIT);
-            pI.setLocation(Context.getLocationService().getDefaultLocation());
-            //pI.setIdentifier(person.getUuid());
-            Random rand = new Random();
-            int randomNum = rand.nextInt((99999-80000)+1) + 800000;
-            pI.setIdentifier(Integer.toString(randomNum));
-            patient.addIdentifier(pI);
-            patientService.savePatient(patient);
-        }
-        model.addAttribute("patient", patient);
+        Patient patient= Context.getPatientService().getPatientByUuid(Context.getAuthenticatedUser().getPerson().getUuid());
+        if (patient == null)
+            model.addAttribute("patient", patient);
+        else
+            model.addAttribute("patient", null);
 
     }
 
